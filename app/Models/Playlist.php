b18e54544f90b6644df9c93ca07d8e79313842bb
Pablo <pablo.galo@iesdonana.org>
2024-04-14 18:10:35 +0200

    ✨feature / añade las relaciones entre los modelos.

--- app/Models/Playlist.php
+++ app/Models/Playlist.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Playlist extends Model
 {
     use HasFactory;
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'playlist_user')->withPivot('role');
+    }
+
+    public function songs()
+    {
+        return $this->belongsToMany(Song::class, 'playlist_song');
+    }
 }
