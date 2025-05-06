075cc77ae09b7311c11adde07c479c34f91aed8e
Pablo <pablo.galo@iesdonana.org>
2025-05-06 10:53:45 +0200

    Añadir relación entre Album y User en la migración

--- database/migrations/2025_03_17_103308_create_albums_table.php
+++ database/migrations/2025_03_17_103308_create_albums_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('albums', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->foreignId('user_id')->constrained()->onDelete('cascade'); // Relación con users
             $table->timestamps();
         });
 
